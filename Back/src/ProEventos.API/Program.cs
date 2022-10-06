517ddba12c3e42663d32c4caeda542165991d693
lihenrodrigues <rolienrodrigues.rj@icloud.com>
2022-10-06 07:54:07 -0300

    Seção 4: Angular

--- Back/src/ProEventos.API/Program.cs
+++ Back/src/ProEventos.API/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddDbContext<DataContext>(
 );
 
 builder.Services.AddControllers();
+builder.Services.AddCors();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -26,6 +27,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
 app.MapControllers();
 
